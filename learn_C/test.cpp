7769e9bbe0ded6bc4d4272a15622ac4b84c24a48
monkeybear <clydebear@163.com>
2017-04-18 15:40:00 +0800

    test.cpp

--- learn_C/test.cpp
+++ learn_C/test.cpp
@@ -1,22 +1,15 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int testconstuse(const int &a,const int &b)
-{
 
-}
-int main(int argc, char **argv)
-{
-  std::vector<int> v;
-  for(int i = 0;i<10;i++)
-  {
-    v.push_back(i);
-  }
-  testvetcor(v);
-  vector<int>::iterator it;
-  for(it = v.begin();it != v.end();it++)
-  {
-    cout<<*it<<" ";
-  }
-    return 0;
+int main() {
+  int j = 43;
+  string s = to_string(j);
+  cout<<s<<endl;
+  double d = stod(s);
+  cout<<d<<endl;
+  string s2 = "pi = 3.14";
+  d = stod(s2.substr(s2.find_first_of("+-.0123456789")));
+  cout<<d<<endl;
+  return 0;
 }
