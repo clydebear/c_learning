e7539e6827fdf37abecba8c05df50ce01f496ce0
monkeybear <clydebear@163.com>
2017-05-05 15:33:52 +0800

    contents

--- learn_C/1test.cpp
+++ learn_C/1test.cpp
@@ -2,17 +2,22 @@
 
 using namespace std;
 
+class StrBlobPtr {
+public:
+    std::string& operator*() const {
+        auto p = check(curr, "dreference past end");
+        return (*p)[curr];
+    }
+
+    std::string* operator->()const {
+        return & this->operator*();
+    }
+    //
+};
+
+
 int main(int argc, char const *argv[])
 {
-  int *p(new int (42));  //p指向动态内存
 
-  auto q=p;  //p和q指向相同的内存
-
-  delete p; //p和q均变为无效
-
-  q=nullptr;  //指出p不再绑定到任何对象
-  int a = 10;
-  q = &a;
-  cout<<*p<<endl;
   return 0;
 }
