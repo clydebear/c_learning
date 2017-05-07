f96114e16be145900ddbc6ba45c9e4405f8e4335
monkeybear <clydebear@163.com>
2017-05-07 15:07:45 +0800

    opp

--- learn_C/1test.cpp
+++ learn_C/1test.cpp
@@ -1,23 +1,21 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+class base
+{
+protected:
+  int a;
+};
 
-class StrBlobPtr {
-public:
-    std::string& operator*() const {
-        auto p = check(curr, "dreference past end");
-        return (*p)[curr];
-    }
-
-    std::string* operator->()const {
-        return & this->operator*();
-    }
-    //
+class derived:public base
+{
+  void fun1(derived &d){cout<<d.a<<endl;}//正确
+  //void derived::fun2(base &b){cout<<b.a};//错误
 };
 
 
 int main(int argc, char const *argv[])
 {
 
-  return 0;
+    return 0;
 }
