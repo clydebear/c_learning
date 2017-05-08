336aa6484d0197e8de5ef194ee94150ffe651d8d
monkeybear <clydebear@163.com>
2017-05-08 13:42:50 +0800

    rm

--- learn_C/1test.cpp
+++ learn_C/1test.cpp
@@ -1,21 +1,13 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-class base
-{
-protected:
-  int a;
-};
-
-class derived:public base
-{
-  void fun1(derived &d){cout<<d.a<<endl;}//正确
-  //void derived::fun2(base &b){cout<<b.a};//错误
-};
 
 
 int main(int argc, char const *argv[])
 {
-
+    bitset<13> b(0xbeef);
+    cout<<b<<endl;
+    bitset<128> bitvec(~0ULL);
+    cout<< bitvec<<endl;
     return 0;
 }
